b6add8eaa158ac77d4eac36fe14075e79ee1eed4
niaeashes <nia.eashes@gmail.com>
2013-10-24 18:24:14 +0900

    update client class

--- classes/client.php
+++ classes/client.php
@@ -5,13 +5,24 @@ namespace Gearman;
 class Client
 {
 	protected static $gearman_client = null;
+	protected static $has_server = false;
 
 	public static function _init()
 	{
 		\Config::load('gearman', true);
 
-		static::$gearman_client = new \GearmanClient();
-		static::$gearman_client->addServer(\Config::get('gearman.host', '127.0.0.1'), \Config::get('gearman.port', '4730'));
+		if ( class_exists('GearmanClient') )
+		{
+			try
+			{
+				static::$gearman_client = new \GearmanClient();
+				static::$has_server = static::$gearman_client->addServer(\Config::get('gearman.host', '127.0.0.1'), \Config::get('gearman.port', '4730'));
+			}
+			catch ( \GearmanException $e )
+			{
+				\Log::error($e->getMessage());
+			}
+		}
 	}
 
 	public static function forge($name, $workload)
@@ -19,6 +30,16 @@ class Client
 		$name = (string) $name;
 		$workload = (string) $workload;
 
-		static::$gearman_client->doBackground($name, $workload);
+		if ( ! empty(static::$gearman_client) and static::$has_server )
+		{
+			try
+			{
+				static::$gearman_client->doBackground($name, $workload);
+			}
+			catch ( \GearmanException $e )
+			{
+				\Log::error($e->getMessage());
+			}
+		}
 	}
 }
